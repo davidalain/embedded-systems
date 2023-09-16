2aad87b611cd5e57e5074ac9bdce7414ac0e7140
Gabriel Carvalho <anjomito26@gmail.com>
2023-09-16 00:12:27 +0000

    update joystic

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std::chrono;
 
 // // Blinking rate in milliseconds
-#define BLINKING_RATE     250ms
+#define BLINKING_RATE     500ms
 
 
 // int main()
@@ -34,19 +34,21 @@ using namespace std::chrono;
 // }
 
 
-InterruptIn button(BUTTON1);
-DigitalOut led(LED1);
+/*InterruptIn button(BUTTON1);
 Timer t;
 int counter = 0;
 int leds[] = {0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0, 0xFE, 0xF6};
 BusOut display(PA_10, PC_7, PA_9, PA_8, PB_10, PB_4, PB_5, PB_3);
 
 void botao_solto() {
+    //printf("click!");
     static unsigned long long timeBefore = 0;
     unsigned long long timeNow = duration_cast<milliseconds>(t.elapsed_time()).count();
     if(timeNow - timeBefore >= 20){
+        //printf("vai incrementar");
         display = leds[counter++ % 10];
     }
+    //printf("%i", counter);
     timeBefore = timeNow;
 }
 
@@ -56,5 +58,27 @@ int main(){
 
     while (true) {          // wait around, interrupts will interrupt this!
         ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
+*/
+
+// Teste Joystick
+int main(){
+    AnalogIn x(PA_1);
+    AnalogIn y(PA_4);
+
+    DigitalIn sw(PA_15);
+    DigitalOut led(LED1);
+
+    while (1) {
+        if(x.read() <= 0.33) {
+            led = 0;
+        } else if(x.read() >= 0.66) {
+            led = 1;
+        } else {
+            led = !led;
+            ThisThread::sleep_for(BLINKING_RATE);                      
+        }
+        printf("%u\n", x.read_u16());
     }
 }
